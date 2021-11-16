9074b7af9498006dd874108755770dbd323bd1e6
seb <sebastienchapuy19@gmail.com>
2021-11-16 18:02:23 +0100

    new version good practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Routes } from 'react-router-dom';
 import { Home } from './Page/Home';
 import store from './redux/store';
 
 const Index = () => {
   return(
     <Router>
-      <Switch>
+      <Routes>
         <Route exact path='/' component={Home}/>
         <Redirect to='/'/>
-      </Switch>
+      </Routes>
     </Router>
   )
 }
